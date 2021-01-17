0b5b5ba5c85efe37b4c24cd02675e87421030aaa
Dmitriy Tikhiy <tikhiy.dmitriy@protonmail.com>
2021-01-17 17:04:35 +0200

    vue lazy load fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .postCss('resources/css/app.css', 'public/css', [
-//         //
-//     ])
+mix.config.webpackConfig.output = {
+    chunkFilename: 'js/[name].bundle.js',
+    publicPath: '/',
+};
 
 mix.setPublicPath('public')
     .js('resources/js/app.js', 'public/js')
